5fd0c988068b7f54256dba75d6738321bd691660
karamfab <130866296+karamfab@users.noreply.github.com>
2024-09-08 00:23:34 +0530

    a

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import pandas as pd
+@st.cache
 
-st.write("""ksfhfh""")
-st.write("""karamveer""")
+def loaddata()
+    df = pd.read_csv("crop.csv")
+    return df
+
+data = loaddata()
+
+
+st.sidebar.header("Filters")
+st.sidebar.multiselect('Select a State', options=data.['State Name'].unique())
